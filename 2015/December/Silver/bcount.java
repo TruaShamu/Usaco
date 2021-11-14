ab36a0690289a8ff64035050edc764b297d9c0f6
Trua Shamu <48630514+TruaShamu@users.noreply.github.com>
2021-11-14 08:04:02 -0600

    Update bcount.java

--- 2015/December/Silver/bcount.java
+++ 2015/December/Silver/bcount.java
@@ -1,49 +1,32 @@
 import java.io.*;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
-/*
-DATE:
-SCORE:
-"Spider bit the mouse, sleep deep..."
- */
-public class Main {
+public class Main2 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new FileReader("bcount.in"));
         PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("bcount.out")));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int cows = Integer.parseInt(st.nextToken());
-        int queries = Integer.parseInt(st.nextToken());
-        int[] hCows = new int[cows + 1]; //1 Holsteins
-        int[] gCows = new int[cows + 1]; // 2 Gurnesy?
-        int[] jCows = new int[cows + 1]; //3 Jersey
-        for (int i = 1; i <= cows; i++) {
-            int id = Integer.parseInt(br.readLine());
-            if (id == 1) {
-                hCows[i] = 1;
+        int n = Integer.parseInt(st.nextToken());
+        int q = Integer.parseInt(st.nextToken());
+        int[][] prefix = new int[4][n + 1];
+        for (int i = 1; i <= n; i++) {
+            int curr = Integer.parseInt(br.readLine());
+            // shift over the prefix sums for each value from 1 to 3
+            for (int j = 1; j <= 3; j++) {
+                prefix[j][i] = prefix[j][i - 1];
             }
-            if (id == 2) {
-                gCows[i] = 1;
-            }
-            if (id == 3) {
-                jCows[i] = 1;
-            }
+            // increment the prefix sum for the number that we read in
+            prefix[curr][i]++;
         }
-        for (int i = 1; i < hCows.length; i++) {
-            gCows[i] += gCows[i - 1];
-            hCows[i] += hCows[i - 1];
-            jCows[i] += jCows[i - 1];
-        }
-        for (int query = 0; query < queries; query++) {
+        System.out.println(Arrays.toString(prefix[1]));
+        System.out.println(Arrays.toString(prefix[2]));
+        System.out.println(Arrays.toString(prefix[3]));
+        for (int i = 0; i < q; i++) {
             st = new StringTokenizer(br.readLine());
-            int lb = Integer.parseInt(st.nextToken());
-            int ub = Integer.parseInt(st.nextToken());
-
-            int h = hCows[ub] - hCows[lb - 1];
-            int g = gCows[ub] - gCows[lb - 1];
-            int j = jCows[ub] - jCows[lb - 1];
-
-            pw.println(h + " " + g + " " + j);
-            System.out.println(h + " " + g + " " + j);
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            pw.println((prefix[1][b] - prefix[1][a - 1]) + " " + (prefix[2][b] - prefix[2][a - 1]) + " " + (prefix[3][b] - prefix[3][a - 1]));
         }
         pw.close();
     }
