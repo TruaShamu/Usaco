6f5f97671091ab90195b241c3301f9e5d5f0b8ce
Trua Shamu <48630514+TruaShamu@users.noreply.github.com>
2021-11-18 16:45:52 -0600

    Update cardgame.java

--- 2015/December/Gold/cardgame.java
+++ 2015/December/Gold/cardgame.java
@@ -5,39 +5,38 @@ import java.io.PrintWriter;
 import java.util.Arrays;
 
 public class Main {
-
-    public static void main(String args[]) throws IOException {
+    public static void main(String[] args) throws IOException {
+        long startTime = System.currentTimeMillis();
         BufferedReader br = new BufferedReader(new FileReader("cardgame.in"));
         PrintWriter pw = new PrintWriter("cardgame.out");
         int n = Integer.parseInt(br.readLine());
-        int[] elsieR1 = new int[n / 2];
-        int[] elsieR2 = new int[n / 2];
+        int[] elsieLeft = new int[n / 2];
+        int[] elsieRight = new int[n / 2];
         for (int i = 0; i < n / 2; i++) {
-            elsieR1[i] = Integer.parseInt(br.readLine());
+            elsieLeft[i] = Integer.parseInt(br.readLine().trim());
         }
         for (int i = 0; i < n / 2; i++) {
-            elsieR2[i] = Integer.parseInt(br.readLine());
+            elsieRight[i] = Integer.parseInt(br.readLine().trim());
         }
         int[] elsie = new int[n];
         for (int i = 0; i < n / 2; i++) {
-            elsie[i] = elsieR1[i];
+            elsie[i] = elsieLeft[i];
         }
         for (int i = n / 2; i < n; i++) {
-            elsie[i] = elsieR2[i - n / 2];
+            elsie[i] = elsieRight[i - n / 2];
         }
-        Arrays.sort(elsieR1);
-        Arrays.sort(elsieR2);
-        Arrays.sort(elsie);
 
-        //Print Arrays
-        System.out.println(Arrays.toString(elsieR1));
-        System.out.println(Arrays.toString(elsieR2));
+        Arrays.sort(elsieLeft);
+        Arrays.sort(elsieRight);
+        Arrays.sort(elsie);
+        System.out.println(Arrays.toString(elsieLeft));
+        System.out.println(Arrays.toString(elsieRight));
         System.out.println(Arrays.toString(elsie));
-        //Make bessie array
+
         int[] bessie = new int[n];
-        int iElsie = 0, iBessie = 0; //Index
+        int iElsie = 0, iBessie = 0;
         for (int cur = 1; cur <= 2 * n; cur++) {
-            if (iBessie == n) break; //Reach end of array
+            if (iBessie == n) break;
             if (iElsie < n && elsie[iElsie] == cur) iElsie++;
             else bessie[iBessie++] = cur;
         }
@@ -46,21 +45,18 @@ public class Main {
         int res = 0;
         iBessie = n / 2;
 
+
         for (int i = 0; i < n / 2; i++) {
-            //find the one that can win against elsie
-            while (iBessie < n && bessie[iBessie] < elsieR1[i]) {
-                iBessie++;
-            }
+            while (iBessie < n && bessie[iBessie] < elsieLeft[i]) iBessie++;
             if (iBessie < n) {
-                //WIN
                 res++;
                 iBessie++;
             }
         }
         iBessie = n / 2 - 1;
         for (int i = n / 2 - 1; i >= 0; i--) {
-            while (iBessie >= 0 && bessie[iBessie] > elsieR2[i]) iBessie--;
-            if (iBessie >= 0) System.out.println(i + " " + iBessie + " " + bessie[iBessie] + " " + elsieR2[i]);
+            while (iBessie >= 0 && bessie[iBessie] > elsieRight[i]) iBessie--;
+            if (iBessie >= 0) System.out.println(i + " " + iBessie + " " + bessie[iBessie] + " " + elsieRight[i]);
             if (iBessie >= 0) {
                 res++;
                 iBessie--;
@@ -70,5 +66,4 @@ public class Main {
         pw.close();
 
     }
-
 }
