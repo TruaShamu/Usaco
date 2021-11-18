90063191fcf5e9753dd6cbbb0f9435a36b7682ca
Trua Shamu <48630514+TruaShamu@users.noreply.github.com>
2021-11-18 16:53:22 -0600

    Update herding.java

--- 2019/February/Silver/herding.java
+++ 2019/February/Silver/herding.java
@@ -1,43 +1,35 @@
 import java.io.*;
 import java.util.Arrays;
 
-public class herding {
-    public static int[] arr;
-    public static int N;
+public class Main {
+    public static int cows;
+    public static int[] cowArray;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new FileReader("herding.in"));
-        PrintWriter pw = new PrintWriter(new FileWriter("herding.out"));
-        N = Integer.parseInt(br.readLine());
-        arr = new int[N];
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
+        PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("herding.out")));
+        cows = Integer.parseInt(br.readLine());
+        cowArray = new int[cows];
+        for (int i = 0; i < cows; i++) {
+            cowArray[i] = Integer.parseInt(br.readLine());
         }
-        Arrays.sort(arr);
-
-        pw.println(min());
-        pw.println(max());
+        Arrays.sort(cowArray);
+        int min = findMin();
+        int max = Integer.max(cowArray[cows - 2] - cowArray[0], cowArray[cows - 1] - cowArray[1]) - (cows - 2);
+        pw.println(min);
+        pw.println(max);
         pw.close();
+
     }
 
-    public static int min() {
-        //The last element is not consecutive.
-        if (arr[N - 2] - arr[0] == N - 2 && arr[N - 1] - arr[N - 2] > 2) return 2;
-        //The first element is not consecutive.
-        if (arr[N - 1] - arr[1] == N - 2 && arr[1] - arr[0] > 2) return 2;
-        int j = 0, best = 0;
-        for (int i = 0; i < N; i++) {
-            while (j < N - 1 && (arr[j + 1] - arr[i]) <= N - 1) {
-                j++;
-            }
+    public static int findMin() {
+        if (cowArray[cows - 2] - cowArray[0] == cows - 2 && cowArray[cows - 1] - cowArray[cows - 2] > 2) return 2;
+        if (cowArray[cows - 1] - cowArray[1] == cows - 2 && cowArray[1] - cowArray[0] > 2) return 2;
+        int i, j = 0, best = 0;
+        for (i = 0; i < cows; i++) {
+            while (j < cows - 1 && cowArray[j + 1] - cowArray[i] <= cows - 1) j++;
             best = Integer.max(best, j - i + 1);
         }
-        return N - best;
-    }
-
-
-    public static int max() {
-        //Use the larger of the two gaps (0,n-2) or (1,n-1) .
-        return Math.max(arr[N - 2] - arr[0] - (N - 2), arr[N - 1] - arr[1] - (N - 2));
+        return cows - best;
     }
 }
